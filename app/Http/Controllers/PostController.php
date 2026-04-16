99657c9bec4f772a30cb5c51fa8d845c9455ca1b
Thainá <thaina.mariano@alunos.unis.edu.br>
2026-04-16 20:54:02 -0300

    Adicionando PostController e rotas CRUD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            ['id' => 1, 'titulo' => 'Post 1']
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json([
+            'mensagem' => 'Post criado com sucesso',
+            'dados' => $request->all()
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        return response()->json([
+            'mensagem' => 'Post atualizado com sucesso',
+            'id' => $id,
+            'dados' => $request->all()
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        return response()->json([
+            'mensagem' => 'Post deletado com sucesso',
+            'id' => $id
+        ]);
+    }
 }
