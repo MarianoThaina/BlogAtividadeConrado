e2cd81b16dbec3b57d46c0c5472ceaf516d44846
Thainá <thaina.mariano@alunos.unis.edu.br>
2026-04-16 20:47:18 -0300

    Adicionando controller e rotas corretas

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+
+
